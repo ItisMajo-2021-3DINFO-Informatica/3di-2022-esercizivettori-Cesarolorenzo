d2549edbf980a959b15a7c5d166b35dc8933eb9d
Cesarolorenzo <98150960+Cesarolorenzo@users.noreply.github.com>
2022-03-18 10:46:46 +0100

    es2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 400;
+            int i = 0;
+            Random generatore = new Random();
+            for (int c = 0; c < dimensione; c++)
+            {
+                int numerocasuale = generatore.Next(0, 101);
+                Console.WriteLine($"{numerocasuale}");
+            }
+        }
+    }
+}
